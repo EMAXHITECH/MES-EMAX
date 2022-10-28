2f17ff6f9d0e51fcac80bdedfd1d158428a14785
EMLTK <tkl@emaxht.co.kr>
2022-10-28 10:25:35 +0900

    PopUp Form Update

---
+++ MES_EMAX/Menu/EquipMenu/PopForm/PopEquipImageForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using System.IO;
+
+namespace SERP
+{
+    public partial class PopEquipImageForm : BaseForm
+    {
+        byte[] sImage = null;
+
+        public PopEquipImageForm(byte[] sImage)
+        {
+            InitializeComponent();
+            this.sImage = sImage;
+        }
+
+        private void PopEquipImageForm_Load(object sender, EventArgs e)
+        {
+            MemoryStream ms = new MemoryStream((byte[])sImage);
+            pictureEdit1.Image = Image.FromStream(ms);
+        }
+    }
+}
