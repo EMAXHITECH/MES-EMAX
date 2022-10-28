3432f8bf24549f631bae06769843a1c7831dd4b8
EMLTK <tkl@emaxht.co.kr>
2022-10-28 10:18:19 +0900

    Equip Update

---
+++ MES_EMAX/Menu/EquipMenu/regEquipDay.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace SERP
+{
+    partial class regEquipDay
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(regEquipDay));
+            this.panel_H = new DevExpress.XtraEditors.PanelControl();
+            this.dt_Equip = new DevExpress.XtraEditors.DateEdit();
+            this.labelControl5 = new DevExpress.XtraEditors.LabelControl();
+            this.gc_Equip = new SERP.GridControlEx();
+            this.gv_Equip = new DevExpress.XtraGrid.Views.Grid.GridView();
+            ((System.ComponentModel.ISupportInitialize)(this.panReg)).BeginInit();
+            this.panReg.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.panButton)).BeginInit();
+            this.panButton.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.panel_H)).BeginInit();
+            this.panel_H.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dt_Equip.Properties.CalendarTimeProperties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dt_Equip.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gc_Equip)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gv_Equip)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_Save
+            // 
+            this.btn_Save.Appearance.BackColor = System.Drawing.Color.White;
+            this.btn_Save.Appearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(85)))), ((int)(((byte)(117)))));
+            this.btn_Save.Appearance.Options.UseBackColor = true;
+            this.btn_Save.Appearance.Options.UseBorderColor = true;
+            this.btn_Save.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("btn_Save.ImageOptions.SvgImage")));
+            this.btn_Save.Click += new System.EventHandler(this.btn_Save_Click);
+            // 
+            // btn_Close
+            // 
+            this.btn_Close.Appearance.BackColor = System.Drawing.Color.White;
+            this.btn_Close.Appearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btn_Close.Appearance.Options.UseBackColor = true;
+            this.btn_Close.Appearance.Options.UseBorderColor = true;
+            this.btn_Close.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btn_Close.ImageOptions.Image")));
+            this.btn_Close.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("btn_Close.ImageOptions.SvgImage")));
+            this.btn_Close.Click += new System.EventHandler(this.btn_Close_Click);
+            // 
+            // btn_Excel
+            // 
+            this.btn_Excel.Appearance.BackColor = System.Drawing.Color.White;
+            this.btn_Excel.Appearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btn_Excel.Appearance.Options.UseBackColor = true;
+            this.btn_Excel.Appearance.Options.UseBorderColor = true;
+            this.btn_Excel.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btn_Excel.ImageOptions.Image")));
+            this.btn_Excel.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("btn_Excel.ImageOptions.SvgImage")));
+            this.btn_Excel.Click += new System.EventHandler(this.btn_Excel_Click);
+            // 
+            // btn_Insert
+            // 
+            this.btn_Insert.Appearance.BackColor = System.Drawing.Color.White;
+            this.btn_Insert.Appearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btn_Insert.Appearance.Options.UseBackColor = true;
+            this.btn_Insert.Appearance.Options.UseBorderColor = true;
+            this.btn_Insert.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("btn_Insert.ImageOptions.SvgImage")));
+            this.btn_Insert.Click += new System.EventHandler(this.btn_Insert_Click);
+            // 
+            // btn_Delete
+            // 
+            this.btn_Delete.Appearance.BackColor = System.Drawing.Color.White;
+            this.btn_Delete.Appearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btn_Delete.Appearance.Options.UseBackColor = true;
+            this.btn_Delete.Appearance.Options.UseBorderColor = true;
+            this.btn_Delete.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("btn_Delete.ImageOptions.SvgImage")));
+            this.btn_Delete.Click += new System.EventHandler(this.btn_Delete_Click);
+            // 
+            // btn_Select
+            // 
+            this.btn_Select.Appearance.BackColor = System.Drawing.Color.White;
+            this.btn_Select.Appearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btn_Select.Appearance.Options.UseBackColor = true;
+            this.btn_Select.Appearance.Options.UseBorderColor = true;
+            this.btn_Select.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("btn_Select.ImageOptions.SvgImage")));
+            this.btn_Select.Click += new System.EventHandler(this.btn_Select_Click);
+            // 
+            // btn_Print
+            // 
+            this.btn_Print.Appearance.BackColor = System.Drawing.Color.White;
+            this.btn_Print.Appearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btn_Print.Appearance.Options.UseBackColor = true;
+            this.btn_Print.Appearance.Options.UseBorderColor = true;
+            this.btn_Print.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("btn_Print.ImageOptions.SvgImage")));
+            // 
+            // panReg
+            // 
+            this.panReg.Size = new System.Drawing.Size(1100, 40);
+            // 
+            // panButton
+            // 
+            this.panButton.Location = new System.Drawing.Point(545, 2);
+            // 
+            // panel_H
+            // 
+            this.panel_H.Controls.Add(this.dt_Equip);
+            this.panel_H.Controls.Add(this.labelControl5);
+            this.panel_H.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel_H.Location = new System.Drawing.Point(0, 40);
+            this.panel_H.Name = "panel_H";
+            this.panel_H.Size = new System.Drawing.Size(1100, 45);
+            this.panel_H.TabIndex = 6;
+            // 
+            // dt_Equip
+            // 
+            this.dt_Equip.EditValue = null;
+            this.dt_Equip.Location = new System.Drawing.Point(114, 10);
+            this.dt_Equip.Name = "dt_Equip";
+            this.dt_Equip.Properties.Appearance.Font = new System.Drawing.Font("나눔바른고딕", 12F, System.Drawing.FontStyle.Bold);
+            this.dt_Equip.Properties.Appearance.Options.UseFont = true;
+            this.dt_Equip.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.dt_Equip.Properties.CalendarTimeProperties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.dt_Equip.Size = new System.Drawing.Size(128, 26);
+            this.dt_Equip.TabIndex = 1;
+            // 
+            // labelControl5
+            // 
+            this.labelControl5.Appearance.Font = new System.Drawing.Font("나눔바른고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.labelControl5.Appearance.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(85)))), ((int)(((byte)(152)))));
+            this.labelControl5.Appearance.Options.UseFont = true;
+            this.labelControl5.Appearance.Options.UseForeColor = true;
+            this.labelControl5.Location = new System.Drawing.Point(23, 13);
+            this.labelControl5.Name = "labelControl5";
+            this.labelControl5.Size = new System.Drawing.Size(73, 19);
+            this.labelControl5.TabIndex = 0;
+            this.labelControl5.Text = "* 가동일자";
+            // 
+            // gc_Equip
+            // 
+            this.gc_Equip.AddRowYN = false;
+            this.gc_Equip.CellFocus = true;
+            this.gc_Equip.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gc_Equip.EnterYN = true;
+            this.gc_Equip.Execl_GB = SERP.GridControlEx.Excel_GB.Update;
+            this.gc_Equip.ExpansionCHK = false;
+            this.gc_Equip.Location = new System.Drawing.Point(0, 85);
+            this.gc_Equip.MainView = this.gv_Equip;
+            this.gc_Equip.MouseWheelChk = true;
+            this.gc_Equip.MultiSelectChk = true;
+            this.gc_Equip.Name = "gc_Equip";
+            this.gc_Equip.PopMenuChk = true;
+            this.gc_Equip.Size = new System.Drawing.Size(1100, 765);
+            this.gc_Equip.TabIndex = 7;
+            this.gc_Equip.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gv_Equip});
+            this.gc_Equip.EditorKeyPress += new System.Windows.Forms.KeyPressEventHandler(this.gc_Equip_EditorKeyPress);
+            // 
+            // gv_Equip
+            // 
+            this.gv_Equip.Appearance.FocusedRow.BackColor = System.Drawing.Color.Transparent;
+            this.gv_Equip.Appearance.FocusedRow.Options.UseBackColor = true;
+            this.gv_Equip.GridControl = this.gc_Equip;
+            this.gv_Equip.Name = "gv_Equip";
+            this.gv_Equip.OptionsClipboard.PasteMode = DevExpress.Export.PasteMode.Update;
+            this.gv_Equip.OptionsCustomization.AllowFilter = false;
+            this.gv_Equip.OptionsCustomization.AllowSort = false;
+            this.gv_Equip.OptionsSelection.MultiSelect = true;
+            this.gv_Equip.OptionsView.ShowAutoFilterRow = true;
+            this.gv_Equip.OptionsView.ShowGroupPanel = false;
+            this.gv_Equip.CellValueChanged += new DevExpress.XtraGrid.Views.Base.CellValueChangedEventHandler(this.gv_Equip_CellValueChanged);
+            // 
+            // regEquipDay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.gc_Equip);
+            this.Controls.Add(this.panel_H);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "regEquipDay";
+            this.Size = new System.Drawing.Size(1100, 850);
+            this.Load += new System.EventHandler(this.regEquipDay_Load);
+            this.Controls.SetChildIndex(this.panReg, 0);
+            this.Controls.SetChildIndex(this.panel_H, 0);
+            this.Controls.SetChildIndex(this.gc_Equip, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.panReg)).EndInit();
+            this.panReg.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.panButton)).EndInit();
+            this.panButton.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.panel_H)).EndInit();
+            this.panel_H.ResumeLayout(false);
+            this.panel_H.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dt_Equip.Properties.CalendarTimeProperties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dt_Equip.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gc_Equip)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gv_Equip)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.PanelControl panel_H;
+        private DevExpress.XtraEditors.DateEdit dt_Equip;
+        private DevExpress.XtraEditors.LabelControl labelControl5;
+        private GridControlEx gc_Equip;
+        private DevExpress.XtraGrid.Views.Grid.GridView gv_Equip;
+    }
+}
